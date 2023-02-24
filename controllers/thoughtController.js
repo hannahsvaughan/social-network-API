f47f4af8a650864b088e924b8094ccef0e7e70d7
hannahsvaughan <hannahvaughan421@gmail.com>
2023-02-24 07:17:35 -0500

    pseudocoded controllers

---
+++ controllers/thoughtController.js
@@ -0,0 +1,16 @@
+const { Thought } = require('../models');
+
+// GET ALL THOUGHTS
+
+// GET ONE THOUGHT BY ID
+
+// POST TO CREATE NEW THOUGHT (don't forget to push the created thought's id to the associated user's thoughts array field)
+
+// PUT TO UPDATE THOUGHT BY ITS ID
+
+// DELETE TO REMOVE THOUGHT BY ITS ID
+
+
+// POST TO CRETE RACTION STORED IN SINGLE THOUGHTS REACTION ARRAY FIELD
+
+// DELETE REACTION BY ID
